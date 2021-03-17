6ad427a28360bce6411ce273e3743fadc397ad2a
TadeuszPle <80619625+TadeuszPle@users.noreply.github.com>
2021-03-17 18:13:45 +0100

    Add files via upload

---
+++ ChessMain.py
@@ -0,0 +1,91 @@
+
+import pygame as p
+
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512  # 400 OTHER GOOD OPTION
+DIMENSION = 8  # Board is 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15  # for animations later on
+IMAGES = {}
+
+
+# Initialize a global dictionary of images. This will be called only once in main
+
+def load_images():
+    # IMAGES['wp'] = p.image.load("images/wp.png")
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "wR", "wN", "wB", "wQ", "wK", "bQ", "bp", "wp"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load('images/' + piece + '.png'), (SQ_SIZE, SQ_SIZE))
+        # note - we can access an image by saying 'IMAGES'['wp']
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH,HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False
+    load_images()
+    sqSelected = () #last click of the user
+    playerClicks = [] #keeps track of player clicks  - two tuples
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+                # mouse handler
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): # clicking twice the same sq deselects everything
+                    sqSelected = ()
+                    playerClicks = []
+                    print('deselected')
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2: # after 2nd click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    if move in validMoves:
+                        moveMade = True
+                        gs.makeMove(move)
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        playerClicks = [sqSelected]
+            # key handler
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z: # undo a move when z is pressed
+                    gs.undoMove()
+                    moveMade = True
+        if moveMade:
+            moveMade = False
+            validMoves = gs.getValidMoves()
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen , board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != '--':
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == "__main__":
+    main()
